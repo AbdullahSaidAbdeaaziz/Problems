8a8e11074d388731f00f8123351d76ecc26b7b50
AbdullahSaidAbdeaaziz <https://github.com/AbdullahSaidAbdeaaziz>
2023-11-08 02:09:37 +0200

    Adding new Problems

--- Problems/Problem.cpp
+++ Problems/Problem.cpp
@@ -1,11 +1,7 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    
-    while (res != nullptr) {
-        cout << res->val << " ";
-        res = res -> next;
-    }
+    // Test problems 
     return 0;
 }
